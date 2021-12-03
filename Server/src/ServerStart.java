2502dc2ea63fca8c3ee714f0ecffc79fc71357e4
CoolboY61 <3047016035@qq.com>
2021-12-04 01:16:33 +0800

    Server服务器，启动器

--- Server/src/ServerStart.java
+++ Server/src/ServerStart.java
@@ -9,6 +9,6 @@ import java.net.UnknownHostException;
  */
 public class ServerStart {
     public static void main(String[] args) throws UnknownHostException {
-        new Thread(new Server("127.0.0.1", "2222", "E:\\非编程\\学校作业（word）\\网络协议分析\\大作业\\ServerFIle\\test2.txt")).start();
+        new Thread(new Server("127.0.0.1", "2222", "E:\\非编程\\学校作业（word）\\网络协议分析\\大作业\\ServerFIle\\test.txt")).start();
     }
 }
